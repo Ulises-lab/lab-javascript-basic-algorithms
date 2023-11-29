000b7648ae902bcba7508e7703286d3019c5fa13
Ulises <yaftemiranda@gmail.com>
2023-11-29 17:59:53 -0600

    Iteration 1 with prompt

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("I'm ready")
-
+const prompt = require('prompt-sync')();
 // Iteration 1: Names and Input
-
-
+let driver = prompt('Give me you name, driver: '), navigator = prompt('Give me you name, navigator: ')
+console.log(`The driver's name is ${driver}\nThe navigator's name is ${navigator}`)
 // Iteration 2: Conditionals
 
 
