bf2214f5c2e4c2101c4a3090716f438b0882525c
Ulises <yaftemiranda@gmail.com>
2023-11-30 10:34:02 -0600

    Iteration 2 done

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const prompt = require('prompt-sync')();
 let driver = prompt('Give me you name, driver: '), navigator = prompt('Give me you name, navigator: ')
 console.log(`The driver's name is ${driver}\nThe navigator's name is ${navigator}`)
 // Iteration 2: Conditionals
-
+if(driver.length>navigator.length){
+	console.log(`The driver has the longest name, it has ${driver.length} characters.`)
+}
+else if(driver.length<navigator.length){
+console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`)
+}
+else{
+console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+}
 
 // Iteration 3: Loops
